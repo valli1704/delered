1a6f47dd454a3e54591a9b2074f6eae0ee17f0fb
valliammai <valliammai836@gmail.com>
2023-11-06 07:41:45 +0530

    Deleted

--- app/config/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    secret : "valli secret key"
- }
--- app/validator/statelist_validate.js
+++
@@ -1,6 +0,0 @@
-const Joi = require('joi');
-
-// module.exports=userValidate
-exports.userValidate = Joi.object().keys({
-    country_id: Joi.required(),
-})
